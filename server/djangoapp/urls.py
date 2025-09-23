6cb00da9137c48b0b67467060a1c12d3a8c0c4db
francesmbah <136343069+fraNCESKAY2006@users.noreply.github.com>
2025-09-23 14:08:53 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,14 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
+    path('get_cars', views.get_cars, name='getcars'),
+]
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Only add static files serving in development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
